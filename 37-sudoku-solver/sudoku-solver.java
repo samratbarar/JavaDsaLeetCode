c157bf9cb6547a1d0acba433cacc94409bdf7e0b
samratbarar <samratbarar7707@gmail.com>
2026-07-24 22:12:05 +0530

    Time: 174 ms (80.08%) | Memory: 43 MB (35.24%) - LeetSync

---
+++ 37-sudoku-solver/sudoku-solver.java
@@ -0,0 +1,67 @@
+class Solution {
+    static boolean solver(char[][] board, int row, int col) {
+        if (row == board.length) {
+            for (char[] boardRow : board) {
+                for (char cell : boardRow) {
+                    System.out.print(cell);
+                }
+
+                System.out.println();
+            }
+            System.out.println();
+            return true;
+        }
+        
+        if (col == board[0].length) {
+            return solver(board, row + 1, 0);
+        }
+
+        if (board[row][col] == '.') {
+            for (int k = 1; k <= 9; k++) {
+                char value = (char)(k + '0');
+                if (isSafe(board, value, row, col)) {
+                    board[row][col] = value;
+
+                    if (solver(board, row, col + 1)) {
+                        return true;
+                    }
+                    
+                    board[row][col] = '.';
+                }
+            }
+        } else {
+            return solver(board, row, col + 1);
+        }
+
+        return false;
+    }
+
+    private static boolean isSafe(char[][] board, char value, int row, int col) {
+        for (int i = 0; i < board.length; i++) {
+            if (board[row][i] == value) {
+                return false;
+            }
+        }
+
+        for (int i = 0; i < board[0].length; i++) {
+            if (board[i][col] == value) {
+                return false;
+            }
+        }
+
+        int gridRowStart = row - (row % 3), gridColStart = col - (col % 3);
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[gridRowStart + i][gridColStart + j] == value) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    public void solveSudoku(char[][] board) {
+        solver(board, 0, 0);
+    }
+}
