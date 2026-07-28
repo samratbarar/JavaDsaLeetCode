531f85e961fd2f13499c0be4b7b203379fec1809
samratbarar <samratbarar7707@gmail.com>
2026-07-28 18:40:28 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (30.57%) - LeetSync

--- 1426-find-n-unique-integers-sum-up-to-zero/find-n-unique-integers-sum-up-to-zero.java
+++ 1426-find-n-unique-integers-sum-up-to-zero/find-n-unique-integers-sum-up-to-zero.java
@@ -3,17 +3,17 @@ class Solution {
         int[] ans = new int[n];
         int uniNum = 1;
         if (n % 2 == 0) {
-            for (int i = 0; i < n; i++) {
-                ans[i++] = uniNum;
-                ans[i] = -1 * uniNum;
+            for (int i = 0; i < n; i += 2) {
+                ans[i] = uniNum;
+                ans[i + 1] = -1 * uniNum;
                 uniNum++;
             }
         } else {
             ans[0] = 0;
 
-            for (int i = 1; i < n; i++) {
-                ans[i++] = uniNum;
-                ans[i] = -1 * uniNum;
+            for (int i = 1; i < n; i += 2) {
+                ans[i] = uniNum;
+                ans[i + 1] = -1 * uniNum;
                 uniNum++;
             }
         }
