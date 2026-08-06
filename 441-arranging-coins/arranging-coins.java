9869a9aa50f24819f0fb11d7d6fa69b82369c6ce
samratbarar <samratbarar7707@gmail.com>
2026-08-06 19:51:21 +0530

    Time: 1 ms (100.00%) | Memory: 42.6 MB (81.86%) - LeetSync

--- 441-arranging-coins/arranging-coins.java
+++ 441-arranging-coins/arranging-coins.java
@@ -4,24 +4,15 @@ class Solution {
 
         while (start <= end) {
             int mid = start + (end - start) / 2;
-            long numN = (long)mid * (mid + 1) / 2;
-            long numNminusOne = (long)(mid - 1) * (mid) / 2;
+            long coins = (long)mid * (mid + 1) / 2;
 
-            if (numN == n) {
-                return mid;
-            }
-
-            if (numN > n && numNminusOne < n) {
-                return (mid - 1);
-            }
-
-            if (numN > n) {
+            if (coins > n) {
                 end = mid - 1;
             } else {
                 start = mid + 1;
             }
         }
 
-        return -1;
+        return end;
     }
 }
