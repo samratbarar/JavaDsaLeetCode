b507d95215f0627097bf2fb49587722e7352adaa
samratbarar <samratbarar7707@gmail.com>
2026-08-11 12:10:08 +0530

    Time: 5 ms (24.83%) | Memory: 45 MB (40.60%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.java
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.java
@@ -7,9 +7,9 @@ class Solution {
             set.add(nums1[i]);
         }
 
-        for (int i = 0; i < nums2.length; i++) {
-            if (set.contains(nums2[i])) {
-                ans.add(nums2[i]);
+        for (int num : nums2) {
+            if (set.contains(num)) {
+                ans.add(num);
             }
         }
 
