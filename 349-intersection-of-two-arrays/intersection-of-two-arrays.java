d7078bce9dfa7be008776fe1d26c359680b3f7ab
samratbarar <samratbarar7707@gmail.com>
2026-08-11 11:52:02 +0530

    Time: 7 ms (14.74%) | Memory: 45 MB (40.60%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.java
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.java
@@ -6,49 +6,22 @@ class Solution {
         Arrays.sort(nums1);
         Arrays.sort(nums2);
 
-        if (nums1.length < nums2.length) {
-            for (int i = 0; i < nums1.length; i++) {
-                int start = 0, end = nums2.length - 1;
-                if (i != 0 && nums1[i] == nums1[i - 1]) {
-                    continue;
-                }
+        int idx2 = 0;
+        for (int i = 0; i < nums1.length; i++) {
+            if (i != 0 && nums1[i] == nums1[i - 1]) {
+                continue;
+            }
 
-                while (start <= end) {
-                    int mid = start + (end - start) / 2;
-
-                    if (nums1[i] == nums2[mid]) {
-                        ans[idx++] = nums1[i];
-                        break;
-                    }
-
-                    if (nums2[mid] < nums1[i]) {
-                        start = mid + 1;
-                    } else {
-                        end = mid - 1;
-                    }
+            for (int j = idx2; j < nums2.length; j++) {
+                if (nums1[i] == nums2[j]) {
+                    ans[idx++] = nums1[i];
+                    idx2 = j + 1;
+                    break;
                 }
             }
-        } else {
-            for (int i = 0; i < nums2.length; i++) {
-                int start = 0, end = nums1.length - 1;
-                if (i != 0 && nums2[i] == nums2[i - 1]) {
-                    continue;
-                }
 
-                while (start <= end) {
-                    int mid = start + (end - start) / 2;
-
-                    if (nums2[i] == nums1[mid]) {
-                        ans[idx++] = nums2[i];
-                        break;
-                    }
-
-                    if (nums1[mid] < nums2[i]) {
-                        start = mid + 1;
-                    } else {
-                        end = mid - 1;
-                    }
-                }
+            if (idx == nums2.length) {
+                break;
             }
         }
 
