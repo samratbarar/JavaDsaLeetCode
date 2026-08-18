ec6cf566d1f40a5204ef38a8cc46b6a11cfae9a8
samratbarar <samratbarar7707@gmail.com>
2026-08-18 23:39:27 +0530

    Time: 2 ms (59.97%) | Memory: 42.1 MB (42.15%) - LeetSync

---
+++ 52-n-queens-ii/n-queens-ii.java
@@ -0,0 +1,72 @@
+class Solution {
+    public boolean isSafe(int row, int col, char[][] board) {
+        // horizontal
+        for (int j = 0; j < board.length; j++) {
+            if (board[row][j] == 'Q') {
+                return false;
+            }
+        }
+
+        // horizontal
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][col] == 'Q') {
+                return false;
+            }
+        }
+
+        // Upper left
+        int r = row;
+        for (int c = col; r >= 0 && c >= 0; r--, c--) {
+            if (board[r][c] == 'Q') {
+                return false;
+            }
+        }
+
+        // Upper right
+        r = row;
+        for (int c = col; r >= 0 && c < board.length; r--, c++) {
+            if (board[r][c] == 'Q') {
+                return false;
+            }
+        }
+
+        // Lower left
+        r = row;
+        for (int c = col; r < board.length && c >=0; r++, c--) {
+            if (board[r][c] == 'Q') {
+                return false;
+            }
+        }
+
+        // Lower right 
+        r = row;
+        for (int c = col; r < board.length && c < board.length; r++, c++) {
+            if (board[r][c] == 'Q') {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public int helper(char[][] board, int col, int count) {
+        if (col == board.length) {
+            return count+1;
+        }
+
+        for (int row = 0; row < board.length; row++) {
+            if (isSafe(row, col, board)) {
+                board[row][col] = 'Q';
+                count = helper(board, col+1, count);
+                board[row][col] = '.';
+            }
+        }
+
+        return count;
+    }
+
+    public int totalNQueens(int n) {
+        char[][] board = new char[n][n];
+        return helper(board, 0, 0);
+    }
+}
