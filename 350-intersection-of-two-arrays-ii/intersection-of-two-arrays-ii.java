bc4dcde78c18d3532bbf94e915f09c258c0360c0
samratbarar <samratbarar7707@gmail.com>
2026-08-18 11:29:56 +0530

    Time: 3 ms (95.21%) | Memory: 45.4 MB (9.83%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.java
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.java
@@ -1,34 +1,23 @@
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
-        HashMap<Integer, Integer> map1 = new HashMap<>();
-        HashMap<Integer, Integer> map2 = new HashMap<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
 
         for (int i = 0; i < nums1.length; i++) {
-            if (map1.containsKey(nums1[i])) {
-                int value = map1.get(nums1[i]);
-                map1.put(nums1[i], value+1);
+            if (map.containsKey(nums1[i])) {
+                int value = map.get(nums1[i]);
+                map.put(nums1[i], value+1);
             } else {
-                map1.put(nums1[i], 1);
-            }
-        }
-
-        for (int i = 0; i < nums2.length; i++) {
-            if (map2.containsKey(nums2[i])) {
-                int value = map2.get(nums2[i]);
-                map2.put(nums2[i], value+1);
-            } else {
-                map2.put(nums2[i], 1);
+                map.put(nums1[i], 1);
             }
         }
 
         ArrayList<Integer> ans = new ArrayList<>();
 
-        for (Map.Entry<Integer, Integer> entry : map1.entrySet()) {
-            if (map2.containsKey(entry.getKey())) {
-                int times = Math.min(entry.getValue(), map2.get(entry.getKey()));
-                for (int j = 0; j < times; j++) {
-                    ans.add(entry.getKey());
-                }
+        for (int num : nums2) {
+            if (map.containsKey(num) && map.get(num) > 0) {
+                ans.add(num);
+                int val = map.get(num);
+                map.put(num, val-1);
             }
         }
 
