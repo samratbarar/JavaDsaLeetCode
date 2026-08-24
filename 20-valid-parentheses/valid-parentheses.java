15c516257f505841706c85bd74f5e3474ed8f6d3
samratbarar <samratbarar7707@gmail.com>
2026-08-25 01:12:25 +0530

    Time: 4 ms (36.60%) | Memory: 43.6 MB (9.83%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char currChar = s.charAt(i);
+
+            if (currChar == '(' || currChar == '{' || currChar == '[') {
+                stack.push(currChar);
+            } else if (currChar == ')') {
+                if (stack.isEmpty() || stack.pop() != '(') {
+                    return false;
+                }
+            } else if (currChar == '}') {
+                if (stack.isEmpty() || stack.pop() != '{') {
+                    return false;
+                }
+            } else if (currChar == ']') {
+                if (stack.isEmpty() || stack.pop() != '[') {
+                    return false;
+                }
+            }
+        }
+
+        if (!stack.isEmpty()) {
+            return false;
+        }
+
+        return true;
+    }
+}
